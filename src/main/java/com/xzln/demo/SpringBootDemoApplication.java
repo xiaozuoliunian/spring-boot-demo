cfa8e3fe88480a2acaec0e1c30eea6e37b044f20
xiaozuoliunian <github@a1865>
2019-05-30 14:37:05 +0800

    Initial commit

---
+++ src/main/java/com/xzln/demo/SpringBootDemoApplication.java
@@ -0,0 +1,25 @@
+package com.xzln.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Administrator
+ */
+@Controller
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "hello world!";
+    }
+}
